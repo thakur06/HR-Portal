6f01d80f57646583554b3b76016c8e66b304f3fd
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2025-04-15 14:44:26 +0530

    added useAuth issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,9 @@ import { Clock } from './Pages/Clock';
 import { Logs } from './Pages/Logs';
 import { Analytics } from './Pages/Analytics';
 import Login from './Pages/Login';
-import { useAuth } from './context/useAuth';
 import { useQuery, useMutation, gql } from "@apollo/client";
 import Profile from './Pages/Profile';
+import { useAuth } from './context/useAuth';
 const CREATE_USER = gql`
   mutation CreateUser($name: String!, $email: String!, $password: String, $role: String) {
     createUser(name: $name, email: $email, password: $password, role: $role) {
